addf83cb0a64e8937e2d4413b5364341906946f1
Sarthak Agrawal <sarthak.agrawal@healthplix.com>
2022-06-15 15:41:37 +0530

    Add CartItem component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import CartItem from './CartItem';
 
 function App() {
   return (
     <div className="App">
-      <h1>Cart</h1>
+      <CartItem />
     </div>
   );
 }
