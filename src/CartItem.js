8839aacbd541ca179a87d0d563fe7e111ee018f8
Sarthak Agrawal <sarthak.agrawal@healthplix.com>
2022-06-15 16:02:34 +0530

    Use destructuring method to destructure state inside render

--- src/CartItem.js
+++ src/CartItem.js
@@ -11,15 +11,16 @@ class CartItem extends React.Component {
         }
     }
     render(){
+        const {price, title, qty} = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block">
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>{this.state.title}</div>
-                    <div style={{color: '#777'}}>Rs. {this.state.price}</div>
-                    <div style={{color: '#777'}}>Qty: {this.state.qty}</div>
+                    <div style={{fontSize:25}}>{title}</div>
+                    <div style={{color: '#777'}}>Rs. {price}</div>
+                    <div style={{color: '#777'}}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
                         <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992651.png" />
