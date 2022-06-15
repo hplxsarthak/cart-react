32cd53c193abb5023b4044c84d9dcb9973c8c000
Sarthak Agrawal <sarthak.agrawal@healthplix.com>
2022-06-15 16:00:27 +0530

    Use state inside cartitem component

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,6 +1,15 @@
 import React from "react";
 
 class CartItem extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            price: 999,
+            title: 'phone',
+            qty: 1,
+            img: ''
+        }
+    }
     render(){
         return (
             <div className="cart-item">
@@ -8,11 +17,15 @@ class CartItem extends React.Component {
                     <img style={styles.image}/>
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize:25}}>Phone</div>
-                    <div style={{color: '#777'}}>Rs. 999</div>
-                    <div style={{color: '#777'}}>Qty: 1</div>
+                    <div style={{fontSize:25}}>{this.state.title}</div>
+                    <div style={{color: '#777'}}>Rs. {this.state.price}</div>
+                    <div style={{color: '#777'}}>Qty: {this.state.qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <img alt="increase" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992651.png" />
+                        <img alt="decrease" className="action-icons" src="https://cdn-icons-png.flaticon.com/128/992/992683.png" />
+                        <img alt="delete" className="action-icons" src="https://cdn-icons.flaticon.com/png/128/3405/premium/3405244.png?token=exp=1655289322~hmac=ba7ddc72e3c568ad41595e6963d766f7" />
+
                     </div>
                 </div>
             </div>
